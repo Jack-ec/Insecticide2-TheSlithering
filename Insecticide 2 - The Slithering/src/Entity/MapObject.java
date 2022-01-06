752e5d3ca51c2c75258bbbb870e2ab9c42786731
jchid <jchid@LAPTOP-LV3MAVIE>
2022-01-05 22:31:20 -0700

    base game completed now need to make it my game

--- Insecticide 2 - The Slithering/src/Entity/MapObject.java
+++ Insecticide 2 - The Slithering/src/Entity/MapObject.java
@@ -101,7 +101,6 @@ public abstract class MapObject {
 		bottomLeft = bl == Tile.BLOCKED;
 		bottomRight = br == Tile.BLOCKED;
 		
-		
 	}
 	
 	public void checkTileMapCollision() {
@@ -197,6 +196,27 @@ public abstract class MapObject {
 			x + xmap - width > GamePanel.WIDTH ||
 			y + ymap + height < 0 ||
 			y + ymap - height > GamePanel.HEIGHT;
+	}
+	
+	public void draw(java.awt.Graphics2D g) {
+		if(facingRight) {
+			g.drawImage(
+				animation.getImage(),
+				(int)(x + xmap - width / 2),
+				(int)(y + ymap - height / 2),
+				null
+			);
+		}
+		else {
+			g.drawImage(
+				animation.getImage(),
+				(int)(x + xmap - width / 2 + width),
+				(int)(y + ymap - height / 2),
+				-width,
+				height,
+				null
+			);
+		}
 	}
 	
 }
--- Insecticide 2 - The Slithering/src/GameState/Level1State.java
+++ Insecticide 2 - The Slithering/src/GameState/Level1State.java
@@ -3,9 +3,12 @@ package GameState;
 import Main.GamePanel;
 import TileMap.*;
 import Entity.*;
+import Entity.Enemies.*;
+import Audio.AudioPlayer;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 public class Level1State extends GameState {
 	
@@ -13,6 +16,13 @@ public class Level1State extends GameState {
 	private Background bg;
 	
 	private Player player;
+	
+	private ArrayList<Enemy> enemies;
+	private ArrayList<Explosion> explosions;
+	
+	private HUD hud;
+	
+	private AudioPlayer bgMusic;
 	
 	public Level1State(GameStateManager gsm) {
 		this.gsm = gsm;
@@ -25,14 +35,43 @@ public class Level1State extends GameState {
 		tileMap.loadTiles("/Tilesets/grasstileset.gif");
 		tileMap.loadMap("/Maps/level1-1.map");
 		tileMap.setPosition(0, 0);
+		tileMap.setTween(1);
 		
 		bg = new Background("/Backgrounds/grassbg1.gif", 0.1);
 		
 		player = new Player(tileMap);
 		player.setPosition(100, 100);
 		
+		populateEnemies();
+		
+		explosions = new ArrayList<Explosion>();
+		
+		hud = new HUD(player);
+		
+		bgMusic = new AudioPlayer("/Music/level1-1.mp3");
+		bgMusic.play();
+		
 	}
 	
+	private void populateEnemies() {
+		
+		enemies = new ArrayList<Enemy>();
+		
+		Slugger s;
+		Point[] points = new Point[] {
+			new Point(200, 100),
+			new Point(860, 200),
+			new Point(1525, 200),
+			new Point(1680, 200),
+			new Point(1800, 200)
+		};
+		for(int i = 0; i < points.length; i++) {
+			s = new Slugger(tileMap);
+			s.setPosition(points[i].x, points[i].y);
+			enemies.add(s);
+		}
+		
+	}
 	
 	public void update() {
 		
@@ -42,6 +81,33 @@ public class Level1State extends GameState {
 			GamePanel.WIDTH / 2 - player.getx(),
 			GamePanel.HEIGHT / 2 - player.gety()
 		);
+		
+		// set background
+		bg.setPosition(tileMap.getx(), tileMap.gety());
+		
+		// attack enemies
+		player.checkAttack(enemies);
+		
+		// update all enemies
+		for(int i = 0; i < enemies.size(); i++) {
+			Enemy e = enemies.get(i);
+			e.update();
+			if(e.isDead()) {
+				enemies.remove(i);
+				i--;
+				explosions.add(
+					new Explosion(e.getx(), e.gety()));
+			}
+		}
+		
+		// update explosions
+		for(int i = 0; i < explosions.size(); i++) {
+			explosions.get(i).update();
+			if(explosions.get(i).shouldRemove()) {
+				explosions.remove(i);
+				i--;
+			}
+		}
 		
 	}
 	
@@ -55,6 +121,21 @@ public class Level1State extends GameState {
 		
 		// draw player
 		player.draw(g);
+		
+		// draw enemies
+		for(int i = 0; i < enemies.size(); i++) {
+			enemies.get(i).draw(g);
+		}
+		
+		// draw explosions
+		for(int i = 0; i < explosions.size(); i++) {
+			explosions.get(i).setMapPosition(
+				(int)tileMap.getx(), (int)tileMap.gety());
+			explosions.get(i).draw(g);
+		}
+		
+		// draw hud
+		hud.draw(g);
 		
 	}
 	
